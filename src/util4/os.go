5128b361a6ab3a7d0d7002c567c55e8e7a75b8c6
zzzzzzzzzzz <zzzzzzzzzzzgg@gmail.com>
2015-10-31 13:21:02 +0800

    加入“改变目录”。“程序名”支持“最终”（找到真实完整文件名）。“重定向输出”、“启动被动者”支持行式参数

--- src/util4/os.go
+++ src/util4/os.go
@@ -1,12 +1,14 @@
 package util4
 
 import (
-	"github.com/zzzzzzzzzzz0/zhscript-go/zhscript"
+	. "github.com/zzzzzzzzzzz0/zhscript-go/zhscript"
 	"os"
 	"os/exec"
 	"io"
+	"path/filepath"
 	"strconv"
 	"fmt"
+	"bytes"
 )
 
 func Errln__(v interface{}) {
@@ -14,8 +16,8 @@ func Errln__(v interface{}) {
 	fmt.Fprintf(os.Stderr, "%v\n", v)
 }
 
-func Errgotoln__(g *zhscript.Goto___) {
-	Errln__(zhscript.New_errinfo__("无法到达目标", g.S, g.Kw))
+func Errgotoln__(g *Goto___) {
+	Errln__(New_errinfo__("无法到达目标", g.S, g.Kw))
 }
 
 type zs_cmd___ struct {
@@ -24,18 +26,24 @@ type zs_cmd___ struct {
 }
 
 func cmd__(s []interface{}, i1 int, s__ func(interface{}) (string, bool)) *exec.Cmd {
-	name, ok := s__(s[i1]); if !ok {return nil}
+	var name string
 	args := []string{}
-	for i := i1 + 1; i < len(s); i++ {
+	for i := i1; i < len(s); i++ {
 		si, ok := s__(s[i]); if !ok {return nil}
-		args = append(args, si)
+		for _, s2 := range Fields__(si) {
+			if name == "" {
+				name = s2
+				continue
+			}
+			args = append(args, s2)
+		}
 	}
 	return exec.Command(name, args...)
 }
 
-func Os__(qv *zhscript.Qv___, k string, s []interface{}, s__ func(interface{}) (string, bool),
+func Os__(qv *Qv___, k string, s []interface{}, s__ func(interface{}) (string, bool),
 err__ func(...interface{}), buzu__ func(int) bool, buzhichi__ func(...interface{}),
-ret__ func(...interface{})) (no_use bool, goto1 *zhscript.Goto___) {
+ret__ func(...interface{})) (no_use bool, goto1 *Goto___) {
 	switch k {
 	case "环境变量":
 		if buzu__(1) {
@@ -44,8 +52,59 @@ ret__ func(...interface{})) (no_use bool, goto1 *zhscript.Goto___) {
 		name, ok := s__(s[0]); if !ok {return}
 		ret__(os.Getenv(name))
 		return
+	case "改变目录":
+		if buzu__(1) {
+			return
+		}
+		name, ok := s__(s[0]); if !ok {return}
+		err := os.Chdir(name)
+		if err != nil {
+			ret__(err)
+		}
+		return
+	case "当前目录":
+		dir, err := os.Getwd()
+		if err != nil {
+			err__(err)
+			return
+		}
+		ret__(dir)
+		return
 	case "程序名":
-		ret__(os.Args[0])
+		ret := os.Args[0]
+		for _, s1 := range s {
+			switch s1 {
+			case "最终":
+				for {
+					if Starts__(ret, ".") {
+						dir, err := os.Getwd()
+						if err == nil {
+							ret = dir + ret[1:]
+						}
+					}
+					dir := filepath.Dir(ret)
+					s2, err := os.Readlink(dir)
+					if err == nil {
+						if !Starts__(s2, "/") {
+							s2 = dir + "/" + s2
+						}
+						ret = s2 + ret[len(dir):]
+					}
+					s2, err = os.Readlink(ret)
+					if err != nil {
+						break
+					}
+					if !Starts__(s2, "/") {
+						s2 = dir + "/" + s2
+					}
+					ret = s2
+				}
+			default:
+				buzhichi__(s1)
+				return
+			}
+		}
+		ret__(ret)
 		return
 	case "重定向输出":
 		if buzu__(1) {
@@ -55,8 +114,11 @@ ret__ func(...interface{})) (no_use bool, goto1 *zhscript.Goto___) {
 		if cmd == nil {
 			return
 		}
-		buf, err := cmd.Output()
-		ret__(string(buf))
+		var buf bytes.Buffer
+		cmd.Stdout = &buf
+		cmd.Stderr = &buf
+		err := cmd.Run()
+		ret__(string(buf.Bytes()))
 		if err != nil {
 			ret__(err)
 		}
@@ -103,7 +165,7 @@ ret__ func(...interface{})) (no_use bool, goto1 *zhscript.Goto___) {
 			return
 		}
 		zc, ok := s[1].(*zs_cmd___); if !ok {
-			zhscript.O__("%v",s[1])
+			O__("%v",s[1])
 			err__(k)
 			return
 		}
