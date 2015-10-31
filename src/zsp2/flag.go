c0b08bca0cf374d5406cfe52148863bb58613005
zzzzzzzzzzz <zzzzzzzzzzzgg@gmail.com>
2015-10-31 13:08:09 +0800

    加-zsp-clpars控制是否允许参数合并。兼容http头侦听地址

--- src/zsp2/flag.go
+++ src/zsp2/flag.go
@@ -40,16 +40,23 @@ func (this *Zsp___) flag2__(a []string, ss *Strings___) *Errinfo___ {
 						break
 					}
 				}
+				if zsp_clpars_ {
+					if !has_value {
+						i1 := i + 1
+						if i1 < len(a) {
+							s1 := a[i1]
+							if s1 != "" && s1[0] == '-' {
+							} else {
+								i++
+								value = a[i]
+								s += "=" + value
+							}
+						}
+					}
+				}
 	
 				f := flag.Lookup(name)
 				if f != nil {
-					if !has_value {
-						i++
-						if i >= len(a) {
-							return New_errinfo__("-" + f.Name, Errs_.Case)
-						}
-						value = a[i]
-					}
 					if err := f.Value.Set(value); err != nil {
 						return New_errinfo__("-" + f.Name, Errs_.Case, value)
 					}
@@ -62,16 +69,26 @@ func (this *Zsp___) flag2__(a []string, ss *Strings___) *Errinfo___ {
 	return nil
 }
 
-func (this *Zsp___) flag__(root *string, is_serve *bool, ss *Strings___) *Errinfo___ {
+var zsp_clpars_ bool
+
+func (this *Zsp___) flag__(root *string, is_serve *bool, ss *Strings___) (err *Errinfo___) {
 	flag.StringVar(root, "r", ".", "根")
 	flag.StringVar(&this.addr, "a", ":0", "侦听地址")
 	flag.StringVar(&this.index, "i", "index.zsp", "索引页")
 	flag.BoolVar(is_serve, "s", true, "做为服务")
+	
+	flag.BoolVar(&zsp_clpars_, "zsp-clpars", true, "")
 
 	//flag.CommandLine.Parse(z.Args.A__())
 	//args.Add__(flag_.Args()...)
 
 	a := os.Args
 	ss.Add__(a[0])
-	return this.flag2__(a[1:], ss)
+	err = this.flag2__(a[1:], ss)
+
+	ht := "http://"
+	if util4.Starts__(this.addr, ht) {
+		this.addr = this.addr[len(ht):]
+	}
+	return
 }
