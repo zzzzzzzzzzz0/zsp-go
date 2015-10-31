3f5cefc98afe472924a649ea80c4ce022df7137c
zzzzzzzzzzz <zzzzzzzzzzzgg@gmail.com>
2015-10-31 13:26:45 +0800

    “正则格”允默认输出$1。“正则代”带序号。调整

--- src/util4/regexp.go
+++ src/util4/regexp.go
@@ -1,13 +1,13 @@
 package util4
 
 import (
-	"github.com/zzzzzzzzzzz0/zhscript-go/zhscript"
+	. "github.com/zzzzzzzzzzz0/zhscript-go/zhscript"
 	"regexp"
 )
 
-func Regexp__(qv *zhscript.Qv___, k string, s []interface{}, s__ func(interface{}) (string, bool),
+func Regexp__(qv *Qv___, k string, s []interface{}, s__ func(interface{}) (string, bool),
 err__ func(...interface{}), buzu__ func(int) bool, buzhichi__ func(...interface{}),
-ret__ func(...interface{})) (no_use bool, goto1 *zhscript.Goto___) {
+ret__ func(...interface{})) (no_use bool, goto1 *Goto___) {
 	re__ := func(i1, i int) *regexp.Regexp {
 		if i >= 0 && buzu__(i) {
 			return nil
@@ -35,46 +35,16 @@ ret__ func(...interface{})) (no_use bool, goto1 *zhscript.Goto___) {
 		}
 		return
 	case "正则格":
-		re := re__(1, 3)
+		re := re__(1, 2)
 		if re == nil {
 			return
 		}
 		s0, ok := s__(s[0]); if !ok {return}
-		s2, ok := s__(s[2]); if !ok {return}
+		s2 := "$1"
+		if len(s) > 2 {
+			s2, ok = s__(s[2]); if !ok {return}
+		}
 		ret__(re.ReplaceAllString(s0, s2))
-		return
-	case "正则代":
-		re := re__(1, 3)
-		if re == nil {
-			return
-		}
-		code, ok := s__(s[2]); if !ok {return}
-		var (
-			err1 *zhscript.Errinfo___
-			buf *zhscript.Buf___
-			kw *zhscript.Keyword___
-			ret string
-		)
-		s0, ok := s__(s[0]); if !ok {return}
-		for _, s1 := range re.FindAllString(s0, -1) {
-			buf, goto1, err1 = Zs2__(code, qv, s1)
-			if err1 != nil {
-				err__(err1)
-				break
-			}
-			ret += buf.S__()
-			kw, goto1 = Goto1__(goto1)
-			if kw == zhscript.Kws_.Continue {
-				continue
-			}
-			if kw == zhscript.Kws_.Break {
-				break
-			}
-			if goto1 != nil {
-				break
-			}
-		}
-		ret__(ret)
 		return
 	case "正则迭":
 		re := re__(1, 3)
@@ -83,9 +53,9 @@ ret__ func(...interface{})) (no_use bool, goto1 *zhscript.Goto___) {
 		}
 		code, ok := s__(s[2]); if !ok {return}
 		var (
-			err1 *zhscript.Errinfo___
-			buf *zhscript.Buf___
-			kw *zhscript.Keyword___
+			err1 *Errinfo___
+			buf *Buf___
+			kw *Keyword___
 			ret string
 		)
 		s0, ok := s__(s[0]); if !ok {return}
@@ -97,10 +67,43 @@ ret__ func(...interface{})) (no_use bool, goto1 *zhscript.Goto___) {
 			}
 			ret += buf.S__()
 			kw, goto1 = Goto1__(goto1)
-			if kw == zhscript.Kws_.Continue {
+			if kw == Kws_.Continue {
 				continue
 			}
-			if kw == zhscript.Kws_.Break {
+			if kw == Kws_.Break {
+				break
+			}
+			if goto1 != nil {
+				break
+			}
+		}
+		ret__(ret)
+		return
+	case "正则代":
+		re := re__(1, 3)
+		if re == nil {
+			return
+		}
+		code, ok := s__(s[2]); if !ok {return}
+		var (
+			err1 *Errinfo___
+			buf *Buf___
+			kw *Keyword___
+			ret string
+		)
+		s0, ok := s__(s[0]); if !ok {return}
+		for i, s1 := range re.FindAllString(s0, -1) {
+			buf, goto1, err1 = Zs2__(code, qv, s1, Itoa__(i + 1))
+			if err1 != nil {
+				err__(err1)
+				break
+			}
+			ret += buf.S__()
+			kw, goto1 = Goto1__(goto1)
+			if kw == Kws_.Continue {
+				continue
+			}
+			if kw == Kws_.Break {
 				break
 			}
 			if goto1 != nil {
