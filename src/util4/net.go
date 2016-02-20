1e4debf06bcc40bb577744592bda7200a9ebc94e
zzzzzzzzzzz <zzzzzzzzzzzgg@gmail.com>
2016-02-21 00:27:37 +0800

    统一

--- src/util4/net.go
+++ src/util4/net.go
@@ -8,8 +8,8 @@ import (
 )
 
 func Net__(qv *Qv___, k string, s []interface{}, s__ func(interface{}) (string, bool),
-err__ func(...interface{}), buzu__ func(int) bool, buzhichi__ func(...interface{}),
-ret__ func(...interface{})) (no_use bool, goto1 *Goto___) {
+err__ func(...interface{}), buzu__ func(int) bool, buzhichi__ func(...interface{}), can_stat__ func(string) bool,
+ret__ func(...interface{}), c *Chan___) (no_use bool, goto1 *Goto___) {
 	switch k {
 	case "urlencode":
 		for _, s1 := range s {
