9ab309cf510b52db3ba4f49696fcc8385ad3edd2
zzzzzzzzzzz <zzzzzzzzzzzgg@gmail.com>
2016-02-21 00:43:43 +0800

    mymy__为接口统一后自加处理的示范

--- src/zsp/main.go
+++ src/zsp/main.go
@@ -2,8 +2,23 @@ package main
 
 import (
 	. "zsp2"
+	. "github.com/zzzzzzzzzzz0/zhscript-go/zhscript"
+	"util4"
 )
 
+func mymy__(qv *Qv___, k string, s []interface{}, s__ func(interface{}) (string, bool),
+err__ func(...interface{}), buzu__ func(int) bool, buzhichi__ func(...interface{}), can_stat__ func(string) bool,
+ret__ func(...interface{}), c *util4.Chan___) (no_use bool, goto1 *Goto___) {
+	switch k {
+	case "啊":
+		ret__("哈")
+		return
+	}
+	no_use = true
+	return
+}
+
 func main() {
+	Add_i__(mymy__)
 	(&Zsp___{}).Z__()
 }
